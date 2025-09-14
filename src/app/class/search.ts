b485a69765b0fbbb24032ec40a85f589a2efb496
3bayti <info@3bayti.com>
2025-09-14 16:00:01 +0100

    Major mobile update

---
+++ src/app/class/search.ts
@@ -0,0 +1,19 @@
+export class Cart {
+  constructor(
+    public id: number,
+    public token: string,
+    public item: number,
+    public product_id: number,
+    public product_name: string,
+    public product_image: string,
+    public description: string,
+    public quantity: number,
+    public store: number,
+    public discount: number,
+    public price: number,
+    public price_formatted: string,
+    public size: string,
+    public color: string,
+    public is_custom: boolean
+  ){  }
+}
